623897ef6e4d533249db1a503723944f34df30c4
Joonas Jauhiainen <joonas.jauhianen@eficode.com>
2018-06-12 14:43:18 +0300

    Added missing imports

--- src/index.js
+++ src/index.js
@@ -9,6 +9,19 @@ import Button from 'Button';
 import Icon from 'Icon';
 import Section, {SectionHeader, SectionText} from 'Section';
 import Typography from 'Typography';
+import H1 from 'H1';
+import H1Underline from 'H1Underline';
+import H2 from 'H2';
+import H3 from 'H3';
+import H4 from 'H4';
+import H5 from 'H5';
+import Header from 'Header';
+import HeaderLogo from 'HeaderLogo';
+import HeroIntro from 'HeroIntro';
+import NavLink from 'NavLink';
+import Paragraph from 'Paragraph';
+import ParagraphBold from 'ParagraphBold';
+import ParagraphSmall from 'ParagraphSmall';
 
 export {
   Box,
@@ -25,7 +38,7 @@ export {
   H4,
   H5,
   Header,
-  HeaderLogom
+  HeaderLogo,
   HeroIntro,
   NavLink,
   Paragraph,

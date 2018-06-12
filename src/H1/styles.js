2f1ec41482004d115662f452563accfd749bf6fc
Joonas Jauhiainen <joonas.jauhianen@eficode.com>
2018-06-12 12:43:54 +0300

    Added missing styles for H1

--- src/H1/styles.js
+++ src/H1/styles.js
@@ -1,10 +1,11 @@
 import styled, { css } from 'styled-components'
 
-export const H1 = styled.div`
+export const H1 = styled.h1`
   color: ${p => p.color};
   font-family: futura-pt-bold, sans-serif, arial;
   font-style: normal;
   font-weight: 700;
-  font-size: 52px;
+  font-size: 2.6em;
+  line-height: 1.7em;
   text-transform: uppercase;
 `

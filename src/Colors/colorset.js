3a6dc3c24c3c8f3ab583373baa1b08ed7a05e619
Amritha Govindarajan <amritha.govindarajan@accenture.com>
2018-06-12 12:27:37 +0300

    added colors

--- src/Colors/colorset.js
+++ src/Colors/colorset.js
@@ -2,5 +2,12 @@ export default {
   dark: '#353740',
   grey: '#747d88',
   darkblue: '#01092F',
-  lightgreen: '#79C8AF'
+  lightgreen: '#79C8AF',
+  curiousBlue: '#1B8CEB',
+  caribbeanGreen: '#00D2B2',
+  amaranth: '#DC1D5E',
+  lightGrey: '#888',
+  disabled: '#E0E0E1',
+  normal: '#FFF',
+  black: '#000'
 };
